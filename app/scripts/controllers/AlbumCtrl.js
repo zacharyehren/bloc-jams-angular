f9bd693d3a385b75cdca6819be4a7242d7753305
Ben Neely <ben.neely@kount.com>
2016-12-12 21:27:11 -0700

    Player bar fixes

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -3,7 +3,7 @@
     this.albumData = Fixtures.getAlbum();
     this.songPlayer = SongPlayer;
   }
-  
+
   angular
   .module('blocJams')
   .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
